9637ede5bf45d43ce457a68f758331b29607cf58
Alessio Rugi <alessiorugi95@gmail.com>
2023-11-24 17:18:37 +0100

    preso riferimento ul in JS

--- js/script.js
+++ js/script.js
@@ -5,3 +5,6 @@ gli elementi della lista individualmente con un ciclo while.
 //creo la lista della spesa
 let userListaSpesa = ["farina", "sale", "lievito di birra", "passata di pomodori", "basilico", "mozzarella", "olio extra vergine d'oliva"];
 console.log(userListaSpesa);
+
+//vado a creare la lista aggiungendo i LI ad HTML
+const listaUL = document.getElementById("lista_spesa");
