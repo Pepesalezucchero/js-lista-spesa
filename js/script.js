c5676c1b19984f5f4d3b2da40b907f83b0895c78
Alessio Rugi <alessiorugi95@gmail.com>
2023-11-24 17:15:51 +0100

    array creato in JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 /* Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano)
 gli elementi della lista individualmente con un ciclo while.
 */
+
+//creo la lista della spesa
+let userListaSpesa = ["farina", "sale", "lievito di birra", "passata di pomodori", "basilico", "mozzarella", "olio extra vergine d'oliva"];
+console.log(userListaSpesa);
